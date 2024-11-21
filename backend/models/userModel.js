f98116163a0bcbecf9a088227d7cf0bc54c2c0c3
Saad Eid <saadeid@saads-air.home>
2024-11-21 12:39:22 -0500

    Addedd bcrypt to salt and hash password upon signup

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const Schema = mongoose.Schema
 
@@ -15,6 +16,22 @@ const userSchema = new Schema({
 })
 
 // static signup method
-userSchema.statics.signup = async (email, password) => {}
+// needs to be a regurlar function and not an arrow function so we can use the 'this' keyword inside of the function
+userSchema.statics.signup = async function (email, password) {
+    // this keyword refers to the model
+    const exists = await this.findOne({ email })
+
+    if (exists){
+        throw Error('Email already in use')
+    }
+
+    // genertate salt
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({ email, password: hash })
+
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
