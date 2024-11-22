b2462bcd3dac3788719cc51844fab7d7bbc5bf11
Saad Eid <saadeid@saads-air.home>
2024-11-22 16:05:54 -0500

    Finished the auth into the react app

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -6,9 +6,14 @@ const {
     deleteWorkout,
     updateWorkout
 } = require('../controller/workoutController')
+const requireAuth = require('../middleware/requireAuth')
 
 // creating a router since we need access to the app express instance from the server.js
 const router = express.Router()
+
+// fire this function before any of the end points
+// requires auth for all endpoints below
+router.use(requireAuth)
 
 // defining api endpoints
 router.get('/', getWorkouts)
